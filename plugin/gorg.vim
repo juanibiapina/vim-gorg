e8a3f8557c3ece53e56c9d71284531ca7704e2aa
Juan Ibiapina <juanibiapina@gmail.com>
2019-08-20 21:15:00 +0200

    feat: Add Plug mapping to edit index page

--- plugin/gorg.vim
+++ plugin/gorg.vim
@@ -2,3 +2,5 @@ if exists("g:loaded_gorg") || &cp
   finish
 endif
 let g:loaded_gorg = 1
+
+nnoremap <Plug>GorgOpenIndex :edit index.md<CR>
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,4 +1,11 @@
-RSpec.describe "Main" do
-  it "works" do
+RSpec.describe "Gorg" do
+
+  it "exposes a Plug mapping for opening the index" do
+    vim.command 'nmap zz <Plug>GorgOpenIndex'
+    vim.feedkeys 'zz'
+
+    filename = vim.command 'echo @%'
+    expect(filename).to eq("index.md")
   end
+
 end
