39459176af84e5e07ad41ffcb2c7f57c3d4f6bd5
Juan Ibiapina <juanibiapina@gmail.com>
2019-08-23 03:16:09 +0200

    feat: Add plug mapping for moving to done
    Done items are moved to a done file for the current day.

--- plugin/gorg.vim
+++ plugin/gorg.vim
@@ -4,3 +4,16 @@ endif
 let g:loaded_gorg = 1
 
 nnoremap <Plug>GorgOpenIndex :edit index.md<CR>
+nnoremap <Plug>GorgMoveDone :call <SID>move_to_done()<CR>
+
+" Move current line to done file for current day
+function! s:move_to_done()
+  let line = getline(".")
+  let current_date = strftime("%Y-%m-%d")
+
+  call mkdir("done", "p")
+
+  exec "silent ! echo " . "\"" . line . "\"" . " >> done/" . current_date . ".md"
+
+  call deletebufline(bufname("%"), line("."))
+endfunction
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,3 +1,5 @@
+require "date"
+
 RSpec.describe "Gorg" do
 
   it "exposes a Plug mapping for opening the index" do
@@ -6,6 +8,26 @@ RSpec.describe "Gorg" do
 
     filename = vim.command 'echo @%'
     expect(filename).to eq("index.md")
+  end
+
+  it "exposes a Plug mapping for moving an item to done" do
+    # given
+    vim.command 'nmap zz <Plug>GorgMoveDone'
+    vim.insert 'Line 1<CR>Line 2'
+    vim.normal
+    vim.feedkeys 'G'
+
+    # when
+    vim.feedkeys 'zz'
+    vim.feedkeys 'zz'
+
+    # then
+    sleep(1.0/10)
+    expect(done_file_contents).to eq("Line 2\nLine 1\n")
+  end
+
+  def done_file_contents
+    File.read("done/#{Date.today.iso8601}.md")
   end
 
 end
